0da6978345f97dc783d253d382e1677c845a206d
Maxim Tartachnik <maxim.tartachnik@gmail.com>
2025-07-11 02:49:52 -0400

    Refactors macro implementation for reducer logging
    Moves the `TCAPrintReducerMacro` implementation to a new file,
    and removes the experimental `LoadingState` macros.
    
    This change streamlines the project structure and focuses on the
    core functionality of reducer logging.

--- Sources/TCAPrintReducer/TCAPrintReducer.swift
+++ Sources/TCAPrintReducer/TCAPrintReducer.swift
@@ -12,16 +12,3 @@ public macro ReducerPrinterLog(
     category: String,
     level: OSLogType = .info
 ) = #externalMacro(module: "TCAPrintReducerMacros", type: "TCAPrintReducerMacro")
-
-@attached(member, names: named(loadingState))
-public macro LoadingState(
-    
-) = #externalMacro(module: "TCAPrintReducerMacros", type: "LoadingStateMacro")
-
-
-@attached(accessor, names: named(init), named(get), named(set))
-@attached(peer, names: prefixed(_))
-public macro LoadingState2(
-    
-) = #externalMacro(module: "TCAPrintReducerMacros", type: "LoadingState2Macro")
-
